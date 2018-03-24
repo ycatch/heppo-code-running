9acfd49b3dd48b6e035e8a0434a9355cf91b5989
Yutaka Catch <y-catch@ja2.so-net.ne.jp>
2018-03-25 00:58:14 +0900

    コメントブロックを調整

--- stages/test_31/js/heppoBlockCode.js
+++ stages/test_31/js/heppoBlockCode.js
@@ -17,5 +17,5 @@ Blockly.JavaScript['run_foward'] = function(block) {
 Blockly.JavaScript['start_comment'] = function(block) {
   var value_comment_string = Blockly.JavaScript.valueToCode(block, 'comment_string', Blockly.JavaScript.ORDER_ATOMIC).replace(/\'+/g, "");
   var code = '// ' + value_comment_string + '\n';
-  return code;
+  return [code, Blockly.JavaScript.ORDER_MEMBER];
 };
